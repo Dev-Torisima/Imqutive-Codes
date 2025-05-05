36bfaea7dafea6559c4689e88c5595a0c2c2f5aa
Dev-Torisima <toridevcon@outlook.jp>
2025-05-05 11:35:25 +0900

    Update ImageConverter.js

--- js/ImageConverter.js
+++ js/ImageConverter.js
@@ -9,6 +9,12 @@ const ToBitmap_Const3 = new Uint8Array([0x00, 0x00, 0x00, 0x00]);
 const ToBitmap_Const4 = new Uint8Array([0x7c, 0x00, 0x00, 0x00]);
 const ToBitmap_Const5 = new Uint8Array([0x01, 0x00, 0x20, 0x00, 0x03, 0x00, 0x00, 0x00]);
 
+const ToIconBitmap_Const1 = new Uint8Array([0x28, 0x00, 0x00, 0x00]);
+const ToIconBitmap_Const2 = new Uint8Array([0x01, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00]);
+const ToIconBitmap_Const3 = new Uint8Array([0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+
+
+//l3 : ImageData
 function ToBitmap(l3)
 {
     let l2 = new Uint8Array(l3.data.buffer);
@@ -35,6 +41,60 @@ rl += 0x7c;
 l1.set(l2, rl);
 
 return l1;
+}
+
+//l3 : ImageData
+function ToIconBitmap(l3)
+{
+    let l5 = new Uint8Array(l3.data.buffer);
+    let l2 = new Uint8Array(l5.length);
+    for (let i = 0; i < l3.height; i++) 
+        {
+        for (let j = 0; j < l3.width; j++) 
+            {
+                let h1 = (j + i * l3.width) * 4;
+                let h2 = (j + (l3.height - i - 1) * l3.width) * 4;
+            l2[h2] = l5[h1 + 2];
+            l2[h2 + 1] = l5[h1 + 1];
+            l2[h2 + 2] = l5[h1];
+            l2[h2 + 3] = l5[h1 + 3];
+        }
+    }
+
+    let l8 = new Uint8Array(Math.ceil(l3.width / 8) * l3.height);
+    for (let i = 0; i < l3.height; i++) 
+        {
+        for (let j = 0; j < l3.width; j++) 
+            {
+                let h1 = (j + i * l3.width) * 4 + 3;
+                let h2 = j / 8 + i * Math.ceil(l3.width / 8);
+                if (l2[h1] <= 0x20)
+                {
+                    l8[h2] += 0b01 << (7 - j % 8);
+                }
+        }
+    }
+
+let l1 = new Uint8Array(0x7c + l2.length + l8.length);
+let rl = 0;
+
+//Info Header
+l1.set(ToIconBitmap_Const1, rl);
+l1.set(ToBytes_u32(l3.width), rl + 4);
+l1.set(ToBytes_i32(l3.height * 2), rl + 8);
+l1.set(ToIconBitmap_Const2, rl + 12);
+l1.set(ToBytes_u32(l2.length + l8.length), rl + 20);
+l1.set(ToIconBitmap_Const3, rl + 24);
+rl += 40;
+
+//ImageData(XOR Data)
+l1.set(l2, rl);
+rl += l2.length;
+
+//AlphaData(And Mask)
+l1.set(l8, rl);
+
+    return l1;
 }
 
 function ToBytes_i32(num)
