52bce0e5b6fc2eae209e369f610d809b3d99b268
Dev-Torisima <toridevcon@outlook.jp>
2025-08-14 10:48:53 +0900

    Update RSS-Sitemap-builder-add.py

--- py/RSS-Sitemap-builder-add.py
+++ py/RSS-Sitemap-builder-add.py
@@ -1,1 +1,18 @@
+# additional codes to zip
 
+import zipfile
+
+
+# to zip
+def zip_folder(folder, output_zip):
+    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder)
+                zipf.write(file_path, arcname)
+
+    print(f"✅ : {output_zip}")
+
+# execute
+zip_folder(folder_path, zip_output_path)
