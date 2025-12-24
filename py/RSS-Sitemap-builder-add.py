738da5256f01b97c4469e2414b2ef2f3df8cb6b0
Dev-Torisima <toridevcon@outlook.jp>
2025-12-24 09:27:40 +0900

    Update RSS-Sitemap-builder-add.py

--- py/RSS-Sitemap-builder-add.py
+++ py/RSS-Sitemap-builder-add.py
@@ -1,10 +1,9 @@
-# RSS-Sitemap-builder-add.py
-# additional codes to zip
+#RSS-Sitemap-builder-add.py
+#additional codes to zip
 
 import zipfile
 
-
-# to zip (RSS-Sitemap-builder.py at 150 lines)
+#to zip (RSS-Sitemap-builder.py at 150 lines)
 def zip_folder(folder, output_zip):
     with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(folder):
@@ -13,5 +12,5 @@ def zip_folder(folder, output_zip):
                 arcname = os.path.relpath(file_path, folder)
                 zipf.write(file_path, arcname)
 
-# execute (RSS-Sitemap-builder.py at 154 lines)
+#execute (RSS-Sitemap-builder.py at 154 lines)
 zip_folder(folder_path, zip_output_path)
